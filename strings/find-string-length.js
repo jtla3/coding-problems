39b7637476981f2ecab134d6cfebbe16d32574aa
Mark Marchant <jtla3@yahoo.com>
2020-01-30 23:52:11 -0800

    add more folders and solved some problems in  string directory

---
+++ strings/find-string-length.js
@@ -0,0 +1,17 @@
+// Find length of string
+// use the length method
+
+const string = str => {
+  return str.length;
+}
+
+const str1 = string('I wish I knew love');
+const str2 = string('love');
+const str3 = string('MarkMarchant');
+const str4 = string('I have to feed my foods before 6pm');
+
+// Testing
+console.log(str1); // 18
+console.log(str2); // 4
+console.log(str3); // 12
+console.log(str4); // 34
